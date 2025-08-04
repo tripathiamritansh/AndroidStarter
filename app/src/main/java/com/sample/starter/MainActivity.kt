eafacc840191387d5979a2f924c58fab1abddc7d
Amritansh Tripathi <amritanshtripathi@gmail.com>
2025-08-03 23:35:01 -0700

    fix import

--- app/src/main/java/com/sample/starter/MainActivity.kt
+++ app/src/main/java/com/sample/starter/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.sample.feed.ui.theme.StarterTheme
-import com.sample.starter.ui.TranslateScreen
+
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
