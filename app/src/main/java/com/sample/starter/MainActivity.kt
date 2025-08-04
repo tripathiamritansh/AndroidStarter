edccf7bec6b42ce77b6340f77025d6434030b68b
Amritansh Tripathi <amritanshtripathi@gmail.com>
2025-08-03 23:30:40 -0700

    Updated Main Activity and theme

--- app/src/main/java/com/sample/starter/MainActivity.kt
+++ app/src/main/java/com/sample/starter/MainActivity.kt
@@ -1,9 +1,24 @@
 package com.sample.starter
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.sample.feed.ui.theme.StarterTheme
+import com.sample.starter.ui.TranslateScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StarterTheme {
+                TranslateScreen()
+            }
+        }
+    }
 
 }
